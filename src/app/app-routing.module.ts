3e448ae4d4be7ec680426f492a5bfa9e4043488c
Parikshit1919 <parikshitpatil1919@gmail.com>
2021-03-27 01:24:52 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { RegisterComponent } from './register/register.component';
+import { StudentLoginComponent } from './student-login/student-login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'' ,component:HomepageComponent},
+  {path:'Home',component:HomepageComponent},
+  {path:'Register',component:RegisterComponent},
+  {path:'Login/Admin',component:AdminLoginComponent},
+  {path:'Login/Student', component:StudentLoginComponent},
+  {path:'About', component:AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
