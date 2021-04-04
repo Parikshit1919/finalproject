938478849bee03ef5b5a1e09bdc54860bf4a0208
Terminator <38761454+Parikshit1919@users.noreply.github.com>
2021-04-04 13:23:27 +0530

    added captcha

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class RegisterComponent implements OnInit {
 
   constructor() { }
+   siteKey:string="6Ldl9ZcaAAAAAAvNdFyM-fTThxY3aVN8S13ns1By";
 
   ngOnInit(): void {
   }
