71cf62318e48379c25531e11b698ae74498e3330
Terminator <38761454+Parikshit1919@users.noreply.github.com>
2021-04-10 18:53:42 +0530

    Added Validation and Registration Service

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, FormsModule, Validators} from "@angular/forms";
+import {Register} from '../register';   
+import { Router } from '@angular/router';
+
+import { AuthService } from '../auth.service';
+
+
+import { NgForm, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';  
 
 @Component({
   selector: 'app-register',
@@ -7,16 +13,86 @@ import {FormBuilder, FormGroup, FormsModule, Validators} from "@angular/forms";
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  public RegisterFormGroup: FormGroup;
-  constructor(private formBuilder: FormBuilder) { }
+  data = false;    
+  
+  massage:string; 
+  studentForm= new FormGroup({
+    Fullname: new FormControl('',[Validators.required]),
+    Student_Email: new FormControl('',[Validators.email,Validators.required]),
+    Student_Password:new FormControl('',[Validators.required,Validators.pattern("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$")]),
+    Mobile: new FormControl('',[Validators.required,Validators.pattern("^[0-9]{10}$")]),
+    City: new FormControl('',[Validators.required]),
+    DOB:new FormControl('',[Validators.required]),
+    State:new FormControl('',[Validators.required]),
+    Qualification:new FormControl('',[Validators.required]),
+    Year_of_completion:new FormControl('',[Validators.required]),
+    recaptcha:new FormControl('',[Validators.required])
+   
+
+  })
+  constructor(private formBuilder: FormBuilder, public registration:AuthService,private router:Router) { }
    siteKey:string="6Ldl9ZcaAAAAAAvNdFyM-fTThxY3aVN8S13ns1By";
 
   ngOnInit(): void 
   {
-    this.RegisterFormGroup = this.formBuilder.group({
-      recaptcha: ['', Validators.required]
-    });
+ 
   }
+  get Fullname()
+  {
+    return this.studentForm.get('Fullname');
+  }
+  get Student_Email()
+  {
+    return this.studentForm.get('Student_Email');
+  }
+  get Student_Password()
+  {
+    return this.studentForm.get('Student_Password');
+  }
+  get Mobile()
+  {
+    return this.studentForm.get('Mobile');
+  }
+  get City()
+
+  {
+    return this.studentForm.get('City');
+  }
+  get DOB()
+  {
+    return this.studentForm.get('DOB');
+  }
+  get State()
+  {
+    return this.studentForm.get('State');
+  }
+  get Qualification()
+  {
+    return this.studentForm.get('Qualification');
+  }
+  get Year_of_completion()
+  {
+    return this.studentForm.get('Year_of_completion');
+  }
+  get recaptcha()
+  {
+    return this.studentForm.get('recaptcha');
+  }
+  onFormSubmit()    
+  {    
+    
+  this.registration.create(this.studentForm.value).subscribe(res => {
+      
+      
+      this.router.navigateByUrl('/Login/Student');
+      
+      
+    });
   
 
+ 
+  }    
+
+
 }
+
