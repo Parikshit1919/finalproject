32a4e6b021bfbb662620af8bcb4ad3ed8251240b
ghostEncoder <codeboyjedi@gmail.com>
2021-04-18 22:38:53 +0530

    quiz optimization

--- src/app/student-exam/student-exam.component.ts
+++ src/app/student-exam/student-exam.component.ts
@@ -25,6 +25,11 @@ export class StudentExamComponent implements OnInit {
   i:number =0;                         // USED TO INTERATE THROUGH THE TEST
   exam:Exam;                          // EXAM OBJECT
   score:string;
+  modifiedFlag:boolean=false;
+  op_a:boolean=false;
+  op_b:boolean=false;
+  op_c:boolean=false;
+  op_d:boolean=false;
 
   constructor(
     public StudentService: StudentService,
@@ -51,21 +56,68 @@ export class StudentExamComponent implements OnInit {
       this.ref.detectChanges();
     });
   }
-
+   /********************************** EXAM FUNCTIONS ******************************************************/
   nextQuestion()
   {
     console.log(this.question.Q_no+this.optionSelected);
-    let answer =new Answers(this.question.Q_no,this.optionSelected,parseInt(localStorage.getItem("exam_id")),parseInt(localStorage.getItem('s_id')))  
-    this.answers.push(answer);
-    console.log(this.answers);
     
+    //CHEKC IF QUESTION IS AREADY ANSWERED
+    this.answers.forEach((question)=>
+    {
+      if(question.id==this.question.Q_no)
+      {
+        console.log("ANSWERED QUESTION DETECTED", question.id);
+        this.answers[this.i]=new Answers(this.question.Q_no,this.optionSelected,parseInt(localStorage.getItem("exam_id")),parseInt(localStorage.getItem('s_id')));
+       
+        console.log(this.answers);
+        this.modifiedFlag=true;
+      }
+     
+    })
+    //IF QUESTION IS MODIFIED DONT ADD AGAIN
+    if(!this.modifiedFlag)
+    {
+      let answer =new Answers(this.question.Q_no,this.optionSelected,parseInt(localStorage.getItem("exam_id")),parseInt(localStorage.getItem('s_id')))  
+      this.answers.push(answer);
+      this.modifiedFlag=false;
+    }
+    //RESET THE MODIFIED FALG
+    this.modifiedFlag=false;
+    console.log(this.answers);
+
     //USED TO INCREMENT THE VALUE OF I TILL LAST QUESTION
     if(this.i<this.noOfQuestions)
     {
       this.i+=1;
       console.log(this.i);
       this.question=this.questions[this.i];
-      
+      try {
+        
+      if(this.answers[this.i].answerSelected=="a")
+      {
+        console.log("CHANGING TO A")
+        this.optionSelected='a';
+      }
+      else if (this.answers[this.i].answerSelected=="b")
+      {
+        console.log("CHANGING TO B")
+        this.optionSelected='b';
+      }
+      else if (this.answers[this.i].answerSelected=="c")
+      {
+        console.log("CHANGING TO C")
+        this.optionSelected='c';
+      }
+      else if (this.answers[this.i].answerSelected=="d")
+      {
+        console.log("CHANGING TO D")
+        this.optionSelected='d';
+      }
+    }
+    catch(e)
+    {
+      console.log("new Question No Previous data");
+    }
       //CHANGE BUTTON TEXT FOR THE LAST QUESITON
       if(this.i==this.noOfQuestions)
       {
@@ -84,15 +136,44 @@ export class StudentExamComponent implements OnInit {
 
     }
   }
+
   //METHOD TO GO TO PREVIOUS QUESTION
   previousQuestion()
   {
     this.i-=1;
     console.log(this.i);
     this.question=this.questions[this.i];
+    this.answers.forEach((question)=>
+    {
+      if(question.id==this.question.Q_no)
+      {
+        console.log("PREVIOUS ANSWERED QUESTION DETECTED", question.id);
+        // this.answers[this.i]=new Answers(this.question.Q_no,this.optionSelected,parseInt(localStorage.getItem("exam_id")),parseInt(localStorage.getItem('s_id')));
+        if(this.answers[this.i].answerSelected=="a")
+        {
+        this.optionSelected='a';
+        }
+        else if (this.answers[this.i].answerSelected=="b")
+        {
+          this.optionSelected='b';
+        }
+        else if (this.answers[this.i].answerSelected=="c")
+        {
+          this.optionSelected='c';
+        }
+        else if (this.answers[this.i].answerSelected=="d")
+        {
+          this.optionSelected='d';
+        }
+        console.log(this.answers);
+       
+      }
+     
+    })
+    this.buttonText="Next Question";
     console.log(this.answers)
-    this.answers.pop();
   }
+
   //AUTO SUBMIT TEST
   onTimerFinished(e:Event){
     if (e["action"] == "done"){
@@ -106,6 +187,7 @@ export class StudentExamComponent implements OnInit {
       });
      }
    }
+
    /********************************** MODAL FUNCTIONS ******************************************************/
 
   //REFRESH PAGE METHOD
